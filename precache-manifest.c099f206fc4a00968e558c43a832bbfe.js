1c4e3adf999cd511dbee49d7c72ab8422f68e25f
Kris de Rycke <derycke.kris@gmail.com>
2019-08-22 15:34:54 +0200

    Updates

---
+++ precache-manifest.c099f206fc4a00968e558c43a832bbfe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dc90558f0c74603d73852ddec9554cb",
+    "url": "/krisderycke/note-app-front-end/index.html"
+  },
+  {
+    "revision": "072a140f5b1f2cf13c56",
+    "url": "/krisderycke/note-app-front-end/static/css/main.344823cf.chunk.css"
+  },
+  {
+    "revision": "511153d0353c4379af22",
+    "url": "/krisderycke/note-app-front-end/static/js/2.3b1382de.chunk.js"
+  },
+  {
+    "revision": "072a140f5b1f2cf13c56",
+    "url": "/krisderycke/note-app-front-end/static/js/main.22e2dd68.chunk.js"
+  },
+  {
+    "revision": "e13ad5174e4d723dd206",
+    "url": "/krisderycke/note-app-front-end/static/js/runtime~main.e47b74d4.js"
+  }
+]);
