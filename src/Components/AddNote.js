7a3a848c0723bc2538ec3b98cb7b1787560392f2
Kris de Rycke <derycke.kris@gmail.com>
2019-08-22 14:19:26 +0200

    add note input

--- src/Components/AddNote.js
+++ src/Components/AddNote.js
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 
 export class AddNote extends Component {
   state = {
-    title: ""
+    title: "",
+    note: ""
   };
   onSubmit = e => {
     e.preventDefault();
@@ -11,18 +12,27 @@ export class AddNote extends Component {
     // afterwards need to set the code to indeed submit to database
   };
   onChange = e => this.setState({ title: e.target.value });
+  // onChangeNote = e => this.this.setState({ note: e.target.value });
 
   render() {
     return (
       <form action="POST" onSubmit={this.onSubmit} style={{ display: "flex" }}>
         <input
-          style={{ flex: "10", padding: "5px" }}
+          style={{ flex: "3", padding: "5px" }}
           type="text"
           name="title"
           placeholder="Add Title"
           onChange={this.onChange}
           value={this.state.title}
         />
+        {/* <input
+          style={{ flex: "3", padding: "5px" }}
+          type="text"
+          name="note"
+          placeholder="Add Note"
+          // onChange={this.onChangeNote}
+          value={this.state.note} */}
+        {/* /> */}
         <input
           type="submit"
           value="Add"
